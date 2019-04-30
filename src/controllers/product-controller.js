278f931068647ff82814ebdcff3a681b4ffe97f0
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-04-30 06:18:35 -0300

    Find product with slug

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -5,7 +5,22 @@ const Product = mongoose.model('Product');
 
 exports.get = (req, res, next) => {
     Product
-        .find({ active: true }, 'title price slug')
+        .find({
+            active: true            // Filtros para pesquisa
+        }, 'title price slug')      // Lista os campos do JSON
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({
+            slug: req.params.slug,
+            active: true
+        }, 'title description price slug tags')
         .then(data => {
             res.status(200).send(data);
         }).catch(e => {
