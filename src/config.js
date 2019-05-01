ef5f6fbfb9f4282fe6779f7b66fe21f6df1fe840
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-05-01 11:29:10 -0300

    Remove public key

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb+srv://cavalsilva:ric456@ndstr-nbqkq.azure.mongodb.net/test?retryWrites=true',
-    sendgridKey: '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',
+    sendgridKey: 'SEND-GRID-KEY',
     containerConnectionString: 'SUA CONNECTION STRING'
 }
