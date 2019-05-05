15f187952abc3dc843935022926ed2e026043bc4
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-05-05 11:07:44 -0300

    Restrict routes only admin

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const controller = require('../controllers/product-controller');
 const authService = require('../services/auth-service');
 
-router.get('/', controller.get);
-router.get('/:slug', controller.getBySlug);
-router.get('/admin/:id', controller.getById);
-router.get('/tags/:tag', controller.getByTag);
+router.get('/', authService.isAdmin, controller.get);
+router.get('/:slug', authService.isAdmin, controller.getBySlug);
+router.get('/admin/:id', authService.isAdmin, controller.getById);
+router.get('/tags/:tag', authService.isAdmin, controller.getByTag);
 router.post('/', authService.isAdmin, controller.post);
 router.put('/:id', authService.isAdmin, controller.put);
 router.delete('/', authService.isAdmin, controller.delete);
